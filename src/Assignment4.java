b8e7ab8165a02ede3bb75af9181ca088d8d6c1e2
SAI-ANIRUDH <anirudh@saisam>
2017-07-03 11:48:36 +0530

    Session 1-Assignment4

---
+++ src/Assignment4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Assignment4 {
+
+	public  static void main(String[] arr) {
+		byte a;
+		short b;
+		int c;
+		long d;
+		float e;
+		double f;
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Please input a");
+a=sc.nextByte();
+System.out.println("Please input b");
+b=sc.nextShort();
+
+c=a+b;// Adding Byte and Short Variables//
+System.out.println(""+c);
+d=b+c; // Adding Short and Int Variables//
+System.out.println(""+d); // Automatic Type Casting //
+e=c+d;
+System.out.println(""+e); // Automatic Type Casting //
+f=d+e;
+System.out.println(""+f); // Automatic Type Casting //
+c=(int)e+(int)f;
+System.out.println(""+c); //Explicit Type Casting //
+	}
+
+}
